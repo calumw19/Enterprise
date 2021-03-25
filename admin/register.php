d825a0ad774a7f25a4b77eeb0d09c00e500d7a34
Calum Watson <55537306+calumw19@users.noreply.github.com>
2021-03-25 12:49:54 +0000

    minor change to edit users

--- admin/register.php
+++ admin/register.php
@@ -123,14 +123,14 @@ include('includes/navbar.php');
         
                 <form action="register_edit.php" method="post">
                     <input type="hidden" name="edit_id" value="<?php echo $row["id"]; ?>">
-                    <button  type="submit" name="edit_btn" class="btn btn-success"> EDIT</button>
+                    <button  type="submit" name="edit_btn" class="btn btn-success"> <img src="img/edit.png" style="width: 20px;"></button>
                 </form>
             </td>
             <td>
 
                 <form action="code.php" method="post">
                   <input type="hidden" name="delete_id" value="<?php echo $row['id']  ?>">
-                  <button type="submit" name="delete_btn" class="btn btn-danger"> DELETE</button>
+                  <button type="submit" name="delete_btn" class="btn btn-danger"><img src="img/remove.png" style="width: 20px;"></button></button>
                 </form>
             </td>
           </tr>
